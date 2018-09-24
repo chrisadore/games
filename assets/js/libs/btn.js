0a05f6ecf703b75680510c67b4fded1d7fdd1e5b
Apoorv Agarwal <aapoorv41@gmail.com>
2018-09-25 00:02:37 +0530

    FIrst commit

---
+++ assets/js/libs/btn.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  const arcadeButton = $('.arcade-button')
+  const backSound = new Howl({
+    src: ['/assets/sounds/button2.mp3']
+  })
+  const arcadeSound = new Howl({
+    src: ['/assets/sounds/button1.mp3']
+  })
+  const gameCardContainer = $('.game')
+  const gameCard = $('.game__card')
+  const backButton = $('.back')
+  var isSmall = $(window).innerWidth() <= 900 ? true : false
+
+  const playSound = function(
+    sound,
+    { vol = 1, seek = 0.2 } = { vol: 1, seek: 0.2 }
+  ) {
+    console.log('TCL: playSound -> sound', sound)
+    sound.seek(seek)
+    sound.volume(vol)
+    if (!sound.playing()) {
+      sound.play()
+    }
+  }
+
+  arcadeButton.on('click', function(e) {
+    e.preventDefault()
+    playSound(arcadeSound, { seek: 0.3 })
+  })
+
+  gameCard.on('click', function(e) {
+    e.preventDefault()
+    playSound(arcadeSound, { seek: 0.3 })
+    const card = $(this).clone()
+    const cardWidth = $(this).innerWidth()
+    const offset = $(this).offset()
+    const gameContainerWidth = gameCardContainer.innerWidth()
+    gameCard.fadeOut()
+    gameCardContainer.append(card)
+    card.css({
+      left: `${offset.left}px`,
+      top: `${offset.top}px)`,
+      position: 'absolute',
+      width: `${cardWidth}px`,
+      transition:
+        'transform 0.3s ease-in , left 0.3s cubic-bezier(0.445, 0.05, 0.55, 0.95)'
+    })
+    card.css({
+      left: `${gameContainerWidth / 2 - cardWidth / 2}px`,
+      transform: 'scale(1.1)'
+    })
+    backButton.addClass('show')
+    backButton.on('click', function() {
+      playSound(backSound, { vol: 0.2, seek: 0.4 })
+      card.on('transitionend', function() {
+        gameCard.fadeIn('slow', function() {
+          card.remove()
+        })
+        card.off('transitionend')
+      })
+      card.css({ left: `${offset.left}px`, transform: 'scale(1)' })
+      $(this).removeClass('show')
+      $(this).off('click')
+    })
+  })
+
+  window.addEventListener('resize', function(e) {
+    console.log($(window).innerWidth())
+  })
+})
