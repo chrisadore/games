0d12cd002644bf95a0225b6e9b74920413f6601c
Apoorv Agarwal <aapoorv41@gmail.com>
2018-10-03 00:55:19 +0530

    made some code changes an added the leaderboard basic layout

--- assets/js/libs/btn.js
+++ assets/js/libs/btn.js
@@ -9,7 +9,13 @@ $(document).ready(function() {
   const gameCardContainer = $('.game')
   const gameCard = $('.game__card')
   const controls = $('.controls-container')
+  const backButton = $('#cancel')
+  const playButton = $('#play')
   var isSmall = $(window).innerWidth() <= 900 ? true : false
+
+  const hideControls = function(e) {
+    controls.removeClass('show')
+  }
 
   const playSound = function(
     sound,
@@ -29,18 +35,22 @@ $(document).ready(function() {
 
   gameCard.on('click', function(e) {
     e.preventDefault()
-    const link = e.currentTarget.href
-    const redirectTimeout = setTimeout(function() {
-      // window.history.pushState(link)
-      // window.location.replace(link)
-    }, 2500)
     playSound(arcadeSound, { seek: 0.3 })
+    const link = e.currentTarget.href
     const card = $(this).clone()
     const cardWidth = $(this).innerWidth()
     const offset = $(this).offset()
     const gameContainerWidth = gameCardContainer.innerWidth()
+    var cardLoader = $(card.children('.game__card--loader')[0])
+    var redirectTimeout
+
+    card.click(function() {
+      e.preventDefault()
+    })
+
     gameCard.fadeOut()
     gameCardContainer.append(card)
+
     card.css({
       left: `${offset.left}px`,
       top: `${offset.top}px)`,
@@ -53,21 +63,33 @@ $(document).ready(function() {
       left: `${gameContainerWidth / 2 - cardWidth / 2}px`,
       transform: 'scale(1.1)'
     })
-    var cardLoader = $(card.children('.game__card--loader')[0])
-    cardLoader.addClass('game__card--loader--show')
+
     controls.addClass('show')
-    controls.on('click', function() {
+
+    playButton.click(function() {
+      playSound(backSound, { vol: 0.2, seek: 0.4 })
+      cardLoader.addClass('game__card--loader--show')
+      redirectTimeout = setTimeout(function() {
+        console.log('TCL: redirectTimeout -> redirectTimeout', redirectTimeout)
+        // window.history.pushState(link)
+        window.location.replace(link)
+      }, 2500)
+      $(this).off('click')
+    })
+
+    backButton.on('click', function() {
+      hideControls()
       playSound(backSound, { vol: 0.2, seek: 0.4 })
       card.on('transitionend', function() {
         gameCard.fadeIn('slow', function() {
           card.remove()
         })
         card.off('transitionend')
-        clearTimeout(redirectTimeout)
+        console.log('TCL: redirectTimeout', redirectTimeout)
       })
       card.css({ left: `${offset.left}px`, transform: 'scale(1)' })
       cardLoader.removeClass('game__card--loader--show')
-      $(this).removeClass('show')
+      clearTimeout(redirectTimeout)
       $(this).off('click')
     })
   })
